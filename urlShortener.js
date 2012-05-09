7978d550ba56900c1c6682be5e31854247c9636d
Wladimir Palant <trev@adblockplus.org>
2012-05-09 12:27:09 +0200

    Added file that was missing in the previous commit

---
+++ urlShortener.js
@@ -0,0 +1,76 @@
+/*
+ * This Source Code is subject to the terms of the Mozilla Public License
+ * version 2.0 (the "License"). You can obtain a copy of the License at
+ * http://mozilla.org/MPL/2.0/.
+ */
+
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
+let handler =
+{
+  classDescription: "install-shortener: protocol handler",
+  get contractID() "@mozilla.org/network/protocol;1?name=" + this.scheme,
+  classID: Components.ID("{6886f820-98f9-11e1-a8b0-0800200c9a66}"),
+
+  init: function()
+  {
+    let registrar = Components.manager.QueryInterface(Ci.nsIComponentRegistrar);
+    registrar.registerFactory(this.classID, this.classDescription, this.contractID, this);
+    onShutdown.add((function()
+    {
+      registrar.unregisterFactory(this.classID, this);
+    }).bind(this));
+  },
+
+  getShortURL: function(url)
+  {
+    return handler.scheme + ":" + (this.urls.push(url) - 1);
+  },
+
+  createInstance: function(outer, iid)
+  {
+    if (outer)
+      throw Cr.NS_ERROR_NO_AGGREGATION;
+    return this.QueryInterface(iid);
+  },
+
+  urls: [],
+  scheme: "install-shortener",
+  defaultPort: -1,
+  protocolFlags:
+      Ci.nsIProtocolHandler.URI_NORELATIVE |
+      Ci.nsIProtocolHandler.URI_NOAUTH |
+      Ci.nsIProtocolHandler.URI_DANGEROUS_TO_LOAD |
+      Ci.nsIProtocolHandler.URI_NON_PERSISTABLE |
+      Ci.nsIProtocolHandler.URI_IS_LOCAL_RESOURCE,
+
+  newURI: function(spec, originCharset, baseURI)
+  {
+    let uri = Cc["@mozilla.org/network/simple-uri;1"].createInstance(Ci.nsIURI);
+    uri.spec = spec;
+    return uri;
+  },
+
+  newChannel: function(uri)
+  {
+    if (!/^\d+$/.test(uri.path))
+      throw Cr.NS_ERROR_FAILURE;
+
+    let index = parseInt(uri.path, 10);
+    let result = this.urls[index];
+    if (typeof result == "undefined")
+      throw Cr.NS_ERROR_FAILURE;
+
+    delete this.urls[index];
+    let channel = Services.io.newChannel(result, null, null);
+    channel.originalURI = uri;
+    return channel;
+  },
+
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIProtocolHandler, Ci.nsIFactory])
+}
+
+handler.init();
+
+exports.getShortURL = handler.getShortURL.bind(handler);
